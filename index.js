d64d79cd23fb666512a9ea2d2123eb0d6e41ae7d
Victor Bejarano <alejandrobjrn4@gmail.com>
2021-04-23 23:56:37 -0500

    019 install pug

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-
 const routes = require('./routes');
+const path = require('path');
 
 // Crear una app de express
 const app = express();
+
+// Habilitar pug
+app.set('view engine', 'pug');
+
+// Añadir la carpeta de las vistas
+app.set('views', path.join(__dirname, './views'));
 
 app.use('/', routes());
 
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,5 @@ const proyectosController = require('../controllers/proyectosController')
 module.exports = function() {
     // Ruta para el home
     router.get('/', proyectosController.proyectosHome);
-    router.get('/nosotros', proyectosController.nosotros);
     return router;
 }
