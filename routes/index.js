a1b5998a4252ac4a60741c5c984dfd5f3803564a
Victor Bejarano <alejandrobjrn4@gmail.com>
2021-04-23 23:36:02 -0500

    017 MVC

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const proyectosController = require('../controllers/proyectosController')
+
 module.exports = function() {
     // Ruta para el home
-    router.get('/', (req, res) => {
-        res.send('Index');
-    });
-    router.get('/nosotros', (req, res) => {
-        res.send('Nosotros');
-    });
+    router.get('/', proyectosController.proyectosHome);
+    router.get('/nosotros', proyectosController.nosotros);
     return router;
 }
