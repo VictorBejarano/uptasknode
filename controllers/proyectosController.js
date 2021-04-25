bd6073d9984e2de09f54ddd83e06482dbb1928ee
Victor Bejarano <alejandrobjrn4@gmail.com>
2021-04-24 22:48:47 -0500

    026 Validate form

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -12,5 +12,26 @@ exports.formularioProyectos = (req, res) => {
 
 exports.nuevoProyecto = (req, res) => {
     // Enviar a la consola lo que el usuario escriba
-    console.log(req.body)
+    // console.log(req.body)
+
+    // Validar que tengamos algo en el input
+    const { nombre } = req.body;
+
+    let errores = [];
+
+    if (!nombre) {
+        errores.push({ 'texto': 'Agregar un Nombre al proyecto' });
+    }
+
+    // Si hay errores
+
+    if (errores.length > 0) {
+        res.render('nuevoProyecto', {
+            nombrePagina: 'Nuevo Proyecto',
+            errores
+        });
+    } else {
+        // No hay errores
+        // Insertar en la BD.
+    }
 }
