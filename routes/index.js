75a50696b241cd64db5c446ff4af2364ad3c66d4
Victor Bejarano <alejandrobjrn4@gmail.com>
2021-04-25 00:12:39 -0500

    037 Express validator

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+// Importar express validator
+
+const { body } = require('express-validator/check');
+
 const proyectosController = require('../controllers/proyectosController')
 
 module.exports = function() {
     // Ruta para el home
     router.get('/', proyectosController.proyectosHome);
     router.get('/nuevo-proyecto', proyectosController.formularioProyectos);
-    router.post('/nuevo-proyecto', proyectosController.nuevoProyecto);
+    router.post('/nuevo-proyecto',
+        body('nombre').not().isEmpty().trim().escape(),
+        proyectosController.nuevoProyecto);
     return router;
 }
